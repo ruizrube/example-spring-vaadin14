91ad9d52e9491ead806ebea65f0eb64edf380932
ivanruizrube <ivanruizrube@192.168.1.133>
2020-12-23 12:30:13 +0100

    Nueva funcionalidad en la vista de Hola Mundo

--- example-spring-vaadin14/src/main/java/es/uca/iw/views/helloworld/HelloWorldView.java
+++ example-spring-vaadin14/src/main/java/es/uca/iw/views/helloworld/HelloWorldView.java
@@ -2,14 +2,15 @@ package es.uca.iw.views.helloworld;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import es.uca.iw.views.main.MainView;
 import com.vaadin.flow.router.RouteAlias;
+
+import es.uca.iw.views.main.MainView;
 
 @Route(value = "hello", layout = MainView.class)
 @PageTitle("Hello World")
@@ -20,15 +21,19 @@ public class HelloWorldView extends HorizontalLayout {
     private TextField name;
     private Button sayHello;
     
-    int variableUno;
-    int variableDos;
-
+  
+    private int variableUno;
+    private int variableDos;
+    
     public HelloWorldView() {
-    	int variableTres;
         setId("hello-world-view");
+        int variableTres;
+        int variableCuatro;
+        
+        H1 h1=new H1("Nueva funcionalidad!");
         name = new TextField("What's your name? Tell me");
         sayHello = new Button("Say hello");
-        add(name, sayHello);
+        add(h1,name, sayHello);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
         sayHello.addClickListener( e-> {
             Notification.show("Hola " + name.getValue());
